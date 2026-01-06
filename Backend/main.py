269333e47585c4fbc45c99a5db0cbeb1b5b826a7
Reissss11 <reisrodrigo49@gmail.com>
2026-01-06 23:22:52 +0000

    Criado as portas e os servidores prontos a fazer implementações

--- Backend/main.py
+++ Backend/main.py
@@ -8,3 +8,10 @@ app = FastAPI(title="FanatikJersey API")
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FanatikJersey API"}
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    
+    port = int(os.getenv("PORT", 9000))
+    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
